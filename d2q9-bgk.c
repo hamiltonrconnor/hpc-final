332b187436fcd2d2650f806d8f37283377971a2a
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-03-08 16:25:22 +0000

    paralell no vec

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -544,11 +544,11 @@ float fusion(const t_param params,  int* restrict  obstacles,soa* restrict grid_
 
     const int ny = params.ny;
     const int nx = params.nx;
-      //#pragma omp parallel for reduction(+:tot_u,tot_cells) schedule(runtime)
+      #pragma omp parallel for reduction(+:tot_u,tot_cells) schedule(runtime)
       for (int jj = 0; jj < ny; jj++)
       {
-        //#pragma omp simd reduction(+:tot_u,tot_cells)
-        #pragma omp simd 
+        #pragma omp simd reduction(+:tot_u,tot_cells)
+        //#pragma omp simd
         for (int ii = 0; ii < nx; ii++)
         {
           __assume_aligned((*grid_ptr).s0,64);
