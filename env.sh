4563dd5b967d923a6f7437dd9f82964a90edb1b1
Connor Hamilton <na19530@bc4login1.bc4.acrc.priv>
2022-03-09 01:10:08 +0000

    final

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 # Add any `module load` or `export` commands that your code needs to
 module load languages/intel/2020-u4
 module load languages/gcc/9.3.0
-
-export OMP_PROC_BIND=true
+export OMP_NUM_THREADS=28
+export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE=static
 # compile and run to this file.
