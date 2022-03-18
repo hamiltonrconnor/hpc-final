bec1010de0fd228ab635ca13dab515403247b44b
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-03-18 12:31:08 +0000

    americans :(

--- hello.c
+++ hello.c
@@ -10,5 +10,5 @@ int main(int argc, char *argv[]){
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   printf("Hello from rank %d of %d",rank,nprocs);
-  MPI_Finalise();
+  MPI_Finalize();
 }
