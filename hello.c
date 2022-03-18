f588398549de16a4717264197ac39b43b3d36269
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-03-18 12:30:13 +0000

    includes

--- hello.c
+++ hello.c
@@ -1,4 +1,6 @@
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
 
@@ -7,6 +9,6 @@ int main(int argc, char *argv[]){
   int nprocs,rank;
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  printf("Hello from rank %d of %d",rank,nprocs)
+  printf("Hello from rank %d of %d",rank,nprocs);
   MPI_Finalise();
 }
