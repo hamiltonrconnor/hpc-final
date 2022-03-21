9918db32c3430f677b6b267df3b8c1f4d8bd1f59
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-03-21 12:33:16 +0000

    switch to mpirun

--- env.sh
+++ env.sh
@@ -5,5 +5,4 @@ export OMP_NUM_THREADS=28
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE=static
-export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
 # compile and run to this file.
