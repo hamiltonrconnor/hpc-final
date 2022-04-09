383fff75f11c307909549f9ece6438c904d700b5
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-09 21:59:56 +0100

    testing stage

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -925,10 +925,10 @@ float fusion(const t_param params, t_speed** cells_ptr, t_speed** tmp_cells_ptr,
       }}
     }
 
-    for (int jj = start; jj < end; jj++)
-    {
-    // for (int jj = 0; jj < params.ny; jj++)
+    // for (int jj = start; jj < end; jj++)
     // {
+    for (int jj = 0; jj < params.ny; jj++)
+    {
       for (int ii = 0; ii < params.nx; ii++)
       {
         for (int kk = 0; kk < NSPEEDS; kk++)
