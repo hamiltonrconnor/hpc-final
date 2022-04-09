58402a186fd6a3e4a418921d7513cfb2fa034942
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-09 22:02:16 +0100

    testing stage

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -564,7 +564,20 @@ float fusion(const t_param params, t_speed** cells_ptr, t_speed** tmp_cells_ptr,
 
 
     t_speed* test_ptr = (t_speed*)malloc(sizeof(t_speed) * (params.ny * params.nx));
-    // for (int jj = start; jj < end; jj++)
+
+    for (int jj = 0; jj < params.ny; jj++)
+    {
+      for (int ii = 0; ii < params.nx; ii++)
+      {
+        for (int kk = 0; kk < NSPEEDS; kk++)
+          {
+        if(test_ptr[ii + jj*params.nx].speeds[kk]!=tmp_cells[ii + jj*params.nx].speeds[kk]){
+          printf("rank: %d jj:%d  ii%d kk:%d\n",rank,jj,ii,kk);
+        }
+      }
+      }
+    }
+    // for (int jj = start; jj < end; jj++)\\
     // {
     printf("rank: %d start: %d end: %d\n",rank,start,end);
     for (int jj = 0; jj < params.ny; jj++)
@@ -927,18 +940,18 @@ float fusion(const t_param params, t_speed** cells_ptr, t_speed** tmp_cells_ptr,
 
     // for (int jj = start; jj < end; jj++)
     // {
-    for (int jj = 0; jj < params.ny; jj++)
-    {
-      for (int ii = 0; ii < params.nx; ii++)
-      {
-        for (int kk = 0; kk < NSPEEDS; kk++)
-          {
-        if(test_ptr[ii + jj*params.nx].speeds[kk]!=tmp_cells[ii + jj*params.nx].speeds[kk]){
-          printf("rank: %d jj:%d  ii%d kk:%d\n",rank,jj,ii,kk);
-        }
-      }
-      }
-    }
+    // for (int jj = 0; jj < params.ny; jj++)
+    // {
+    //   for (int ii = 0; ii < params.nx; ii++)
+    //   {
+    //     for (int kk = 0; kk < NSPEEDS; kk++)
+    //       {
+    //     if(test_ptr[ii + jj*params.nx].speeds[kk]!=tmp_cells[ii + jj*params.nx].speeds[kk]){
+    //       printf("rank: %d jj:%d  ii%d kk:%d\n",rank,jj,ii,kk);
+    //     }
+    //   }
+    //   }
+    // }
     return tot_u / (float)tot_cells;
 
 
