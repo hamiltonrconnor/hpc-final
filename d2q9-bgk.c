1810fb55cc7eadb9d47349a267127b44333d55fb
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-10 15:51:48 +0100

    adding test case

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -211,6 +211,7 @@ int main(int argc, char* argv[])
         if(rank==0){
           posLeft=(params.ny-1);
         }
+        printf("Rank: %d memLeft: %d "rank,memLeft);
         for (int ii = 0; ii < params.nx; ii++)
         {
           for (int kk = 0; kk < NSPEEDS; kk++)
