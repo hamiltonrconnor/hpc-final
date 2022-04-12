89e19cfd023038757d92cd6895cfc18a0beb3d1a
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-12 23:22:29 +0100

    tracking down segfault

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -169,10 +169,6 @@ int main(int argc, char* argv[])
   init_tic=tot_tic;
   initialise(paramfile, obstaclefile, &params, &cells, &tmp_cells, &obstacles, &av_vels);
 
-  t_speed* test_cells     = NULL;    /* grid containing fluid densities */
-  t_speed* test_tmp_cells = NULL;    /* scratch space */
-  t_speed** test_cells_ptr = &test_cells;
-  t_speed** test_tmp_cells_ptr= &test_tmp_cells;
 
   //initialise(paramfile, obstaclefile, &params, &test_cells, &test_tmp_cells, &obstacles, &av_vels);
 
@@ -211,16 +207,16 @@ int main(int argc, char* argv[])
     int left = (rank == 0) ? (rank + nprocs - 1) : (rank - 1);
 
 
-    printf("rank: %d tt:%d 2\n",rank,tt);
-    printf("rank: %d tt:%d send:%d 2\n",rank,tt,local_cells[1*params.nx].speeds[0]);
-    printf("rank: %d tt:%d recv: %d2\n",rank,tt,local_cells[work*params.nx].speeds[0]);
+    // printf("rank: %d tt:%d 2\n",rank,tt);
+    // printf("rank: %d tt:%d send:%d 2\n",rank,tt,local_cells[1*params.nx].speeds[0]);
+    // printf("rank: %d tt:%d recv: %d2\n",rank,tt,local_cells[work*params.nx].speeds[0]);
     MPI_Barrier(MPI_COMM_WORLD);
     //printf("rank: %d tt:%d local_cells: %d end:%d buffSize:%d\n",rank,tt,sizeof(t_speed) * ((work+2) * params.nx),end*params.nx,buffSize*sizeof(float));
-    // MPI_Sendrecv(&local_cells[1*params.nx],buffSize , MPI_FLOAT, left, tag,
-    //     &local_cells[work*params.nx],  buffSize ,  MPI_FLOAT, right, tag, MPI_COMM_WORLD, &status);
-    // printf("rank: %d tt:%d 3\n",rank,tt);
-    // MPI_Sendrecv(&local_cells[(work-1)*params.nx],buffSize , MPI_FLOAT, right, tag,
-    //     &local_cells[0],  buffSize ,  MPI_FLOAT, left, tag, MPI_COMM_WORLD, &status);
+    MPI_Sendrecv(&local_cells[1*params.nx],buffSize , MPI_FLOAT, left, tag,
+        &local_cells[work*params.nx],  buffSize ,  MPI_FLOAT, right, tag, MPI_COMM_WORLD, &status);
+    //printf("rank: %d tt:%d 3\n",rank,tt);
+    MPI_Sendrecv(&local_cells[(work-1)*params.nx],buffSize , MPI_FLOAT, right, tag,
+        &local_cells[0],  buffSize ,  MPI_FLOAT, left, tag, MPI_COMM_WORLD, &status);
 
 
 
