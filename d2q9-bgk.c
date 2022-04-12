5a7017cc986603c8a5a7f9692de62505738e2f80
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-12 23:28:32 +0100

    tracking down segfault

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -220,18 +220,19 @@ int main(int argc, char* argv[])
 
 
 
-    // av_vels[tt] = timestep(params, cells_ptr, tmp_cells_ptr, obstacles);
-    // t_speed** temp = cells_ptr;
-    // cells_ptr= tmp_cells_ptr;
-    // tmp_cells_ptr= temp;
+    av_vels[tt] = timestep(params, cells_ptr, tmp_cells_ptr, obstacles);
+    t_speed** temp = cells_ptr;
+    cells_ptr= tmp_cells_ptr;
+    tmp_cells_ptr= temp;
 
     av_vels[tt] = halo_timestep(params, local_cells_ptr, local_tmp_cells_ptr, local_obstacles);
     t_speed** local_temp = local_cells_ptr;
     local_cells_ptr= local_tmp_cells_ptr;
     local_tmp_cells_ptr= local_temp;
-    // printf("After Memcompare left Rank:%d result: %d\n",rank,memcmp(&test_cells[posLeft*params.nx],&cells[posLeft*params.nx],buffSize*sizeof(float)));
-    // printf("After Memcompare mid Rank:%d result: %d\n",rank,memcmp(&test_cells[start*params.nx],&cells[start*params.nx],buffSize*sizeof(float)*work));
-    // printf("After Memcompare right Rank:%d result: %d\n",rank,memcmp(&test_cells[posRight*params.nx],&cells[posRight*params.nx],buffSize*sizeof(float)));
+    printf("After Memcompare left Rank:%d result: %d\n",rank,memcmp(&local_cells[0],&cells[posLeft*params.nx],buffSize*sizeof(float)));
+    printf("After Memcompare mid Rank:%d result: %d\n",rank,memcmp(&test_cells[1*params.nx],&cells[start*params.nx],buffSize*sizeof(float)*work));
+    printf("After Memcompare right Rank:%d result: %d\n",rank,memcmp(&test_cells[work*params.nx],&cells[posRight*params.nx],buffSize*sizeof(float)));
+
     printf("rank: %d tt:%d 5\n",rank,tt);
     //MPI_Barrier(MPI_COMM_WORLD);
 
@@ -645,7 +646,7 @@ float halo_fusion(const t_param params, t_speed** cells_ptr, t_speed** tmp_cells
 
     //Intialiase local cells
 
-  
+
 
 
     for (int jj =1; jj < work+1; jj++)
