eaf3ac975bf27ad90ec6c47aa293ae10eb5a6abb
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-13 00:57:22 +0100

    tracking down segfault

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -300,25 +300,25 @@ int main(int argc, char* argv[])
     //
     //
     //
-    int flag;
-    for (int jj =1; jj < work+1; jj++)
-    {
-      flag = 0;
-      for (int ii = 0; ii < params.nx; ii++)
-      {
-        for (int kk = 0; kk < NSPEEDS; kk++)
-        {
-          if(cells[ii + (jj-1)*params.nx +start*params.nx].speeds[kk] !=local_cells[ii + jj*params.nx].speeds[kk] ){
-            flag =1;
-
-          }
-        }
-
-      }
-      if(flag==1){
-        printf("Rank: %d jj: %d\n",rank,jj);
-      }
-    }
+    // int flag;
+    // for (int jj =1; jj < work+1; jj++)
+    // {
+    //   flag = 0;
+    //   for (int ii = 0; ii < params.nx; ii++)
+    //   {
+    //     for (int kk = 0; kk < NSPEEDS; kk++)
+    //     {
+    //       if(cells[ii + (jj-1)*params.nx +start*params.nx].speeds[kk] !=local_cells[ii + jj*params.nx].speeds[kk] ){
+    //         flag =1;
+    //
+    //       }
+    //     }
+    //
+    //   }
+    //   if(flag==1){
+    //     printf("Rank: %d jj: %d\n",rank,jj);
+    //   }
+    // }
 
 
     //av_vels[tt] = av_velocity(params, cells, obstacles);
