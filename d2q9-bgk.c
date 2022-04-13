0b4dcdb2c3f3112c254001e096c3ee843bf3d474
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-13 17:19:37 +0100

    tracking down segfault

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -205,7 +205,7 @@ int main(int argc, char* argv[])
 
   // for (int tt = 0; tt < params.maxIters; tt++)
   // {
-  for (int tt = 0; tt < 2; tt++)
+  for (int tt = 0; tt < 10; tt++)
   {
     //printf("rank: %d tt:%d 1\n",rank,tt);
     //Init local regions
