c2ce53e98d034d1bfa64f3063826731e060815d5
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-13 17:20:11 +0100

    tracking down segfault

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -190,18 +190,18 @@ int main(int argc, char* argv[])
   int* local_obstacles = malloc(sizeof(int) * (work * params.nx));
 
   memcpy(&local_obstacles[0],&obstacles[start*params.nx],sizeof(int) * (work * params.nx));
-  for(int i = 0;i<(work+2) * params.nx;i++){
-    local_cells[i].speeds[0] = cells ;
-    local_cells[i].speeds[1] = rank;
-    local_cells[i].speeds[2] = rank;
-    local_cells[i].speeds[3] = rank;
-    local_cells[i].speeds[4] = rank;
-    local_cells[i].speeds[5] = rank;
-    local_cells[i].speeds[6] = rank;
-    local_cells[i].speeds[7] = rank;
-    local_cells[i].speeds[8] = rank;
-
-  }
+  // for(int i = 0;i<(work+2) * params.nx;i++){
+  //   local_cells[i].speeds[0] = cells ;
+  //   local_cells[i].speeds[1] = rank;
+  //   local_cells[i].speeds[2] = rank;
+  //   local_cells[i].speeds[3] = rank;
+  //   local_cells[i].speeds[4] = rank;
+  //   local_cells[i].speeds[5] = rank;
+  //   local_cells[i].speeds[6] = rank;
+  //   local_cells[i].speeds[7] = rank;
+  //   local_cells[i].speeds[8] = rank;
+  //
+  // }
 
   // for (int tt = 0; tt < params.maxIters; tt++)
   // {
