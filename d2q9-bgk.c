416839b31b9350d6fa2c20462543e91a7f0e326e
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-16 23:16:07 +0100

    making parallel

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -404,7 +404,7 @@ int main(int argc, char* argv[])
   //print_fushion(params,*cells_ptr);
   //print_halo_fushion(params,*local_cells_ptr,work);
 print_halo_fushion(params,*local_cells_ptr,work);
-print_halo_fushion(params,local_cells,work);
+//print_halo_fushion(params,local_cells,work);
   MPI_Barrier(MPI_COMM_WORLD);
 
   t_speed* output= (t_speed*)malloc(sizeof(t_speed) * params.nx*params.ny);
