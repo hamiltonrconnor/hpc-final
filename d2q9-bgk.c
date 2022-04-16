8f988cc60cfd797f04795aef7b14cde1946df1b0
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-16 21:09:50 +0100

    making parallel

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -406,9 +406,12 @@ int main(int argc, char* argv[])
   t_speed* output= (t_speed*)malloc(sizeof(t_speed) * (params.ny * params.nx));
   print_halo_fushion(params,*local_cells_ptr,work);
   float temp = rank/2;
-  int array[4]
+  float array[4];
   MPI_Gather(&temp,1,MPI_FLOAT,array,1,MPI_FLOAT,0,MPI_COMM_WORLD);
-  print_fushion(params,output);
+  for(int i = 0;i<4;i++){
+    printf("%f",array[i]);
+  }
+  //print_fushion(params,output);
   //
   // int tag = 0;
   // MPI_Status status;
