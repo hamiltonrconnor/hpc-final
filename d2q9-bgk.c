30f11cb56a1243ce966ec48d2893048eaac4de3f
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-20 00:27:45 +0100

    Sorting gatherv

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -437,7 +437,7 @@ int j;
 int b =2;
 for(j = 0;j<nprocs;j++){
   b=2;
-   displs[j] = params.nx*findStart(N,nprocs,j);
+   displs[j] = params.nx*NSPEEDS*findStart(N,nprocs,j);
 
   rcounts[j] = params.nx*NSPEEDS;
 
