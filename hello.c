14bd80a642b0eb66bb9d77b48fd79cdc1e176481
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-20 01:33:30 +0100

    Sorting gatherv

--- hello.c
+++ hello.c
@@ -11,22 +11,6 @@ int main(int argc, char *argv[]){
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   printf("Hello from rank %d of %d\n",rank,nprocs);
 
-  int N = params.ny;
-  int work = N / nprocs;
-  int start = rank * work;
-  int end = start + work;
-
-  //Find the neigbours
-  int right = (rank + 1) % nprocs;
-  int left = (rank == 0) ? (rank + nprocs - 1) : (rank - 1);
-
-  //Get the right data
-  int numElements = (end - start + 1)
-  int initAddress = cells + start;
-  MPI_Sendrecv(cells + start, params.nx , MPI_FLOAT, left, tag,
-      cells+end, params.nx,  MPI_FLOAT right, tag, MPI_COMM_WORLD, &status);
- MPI_Sendrecv(cells + end, params.nx , MPI_FLOAT, right, tag,
-      cells+start, params.nx,  MPI_FLOAT left, tag, MPI_COMM_WORLD, &status);
 
 
 
