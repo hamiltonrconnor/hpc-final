0e0463229c08d91b90172903c3774baa4b97f380
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-21 12:50:35 +0100

    Starting to track down bug again

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 # Add any `module load` or `export` commands that your code needs to
 module load languages/intel/2020-u4
 module load languages/gcc/9.3.0
+module load tools/tau/2.28.2-intel-mpi
 export OMP_NUM_THREADS=28
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
