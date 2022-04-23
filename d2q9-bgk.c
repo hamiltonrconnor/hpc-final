953a0fa19bb6dbc1037656a1412fa592d385e0c9
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-23 10:51:02 +0100

    tracking down work = 1 bug

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -243,7 +243,7 @@ int main(int argc, char* argv[])
     int x =local_obstacles[i];
     snprintf(buf,12,"%d   ",x);
     //printf("%s", buf);
-    strcat(local_matrix),buf);
+    strcat(local_matrix,buf);
     // char space[2] ="  ";
     // strcat(matrix,space);
   }
