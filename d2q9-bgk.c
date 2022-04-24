53764debd3dd257ae63631e6cefe1ecb0dd23e22
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-24 18:22:06 +0100

    trying to find work = 1

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -232,11 +232,11 @@ int main(int argc, char* argv[])
 // }
 if(rank>params.ny/2-1){
   MPI_Finalize();
-     //exit(0);
+     exit(0);
   }
-  nprocs = params.ny/2;
-  // MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-  // MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  //nprocs = params.ny/2;
+  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 
 
