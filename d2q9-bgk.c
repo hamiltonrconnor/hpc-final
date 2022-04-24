6052a0366b7ba81921045670398d29a713a577e1
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-24 17:39:25 +0100

    trying to find work = 1

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -205,17 +205,17 @@ int main(int argc, char* argv[])
   if(params.ny/2<nprocs)nprocs = params.ny/2;
 
   // Obtain the group of processes in the world communicator
-  MPI_Group world_group;
-  MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+MPI_Group world_group;
+MPI_Comm_group(MPI_COMM_WORLD, &world_group);
 
-  // Remove all unnecessary ranks
-  MPI_Group new_group;
-  int ranges[3] = { params.ny/2+1, nprocs-1, 1 };
-  MPI_Group_range_excl(world_group, 1, ranges, &new_group);
+// Remove all unnecessary ranks
+MPI_Group new_group;
+int ranges[3] = { nprocs, nprocs-1, 1 };
+MPI_Group_range_excl(world_group, 1, ranges, &new_group);
 
-  // Create a new communicator
-  MPI_Comm newworld;
-  MPI_Comm_create(MPI_COMM_WORLD, new_group, &newworld);
+// Create a new communicator
+MPI_Comm newworld;
+MPI_Comm_create(MPI_COMM_WORLD, new_group, &newworld);
 
 
   printf("Rank: %d nprocs: %d\n",rank,nprocs);
