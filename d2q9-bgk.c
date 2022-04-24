28cbae89be38bd7174429a201c1a8258c843e92a
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-24 18:12:42 +0100

    trying to find work = 1

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -222,7 +222,8 @@ int main(int argc, char* argv[])
 // }else{
 //   color = 0;
 // }
-// MPI_Comm_split(MPI_COMM_WORLD, color, 0, &new_comm);
+int color = 0;
+MPI_Comm_split(MPI_COMM_WORLD, color, 0, &new_comm);
 // if (new_comm== MPI_COMM_NULL)
 // {
 //    // Bye bye cruel world
