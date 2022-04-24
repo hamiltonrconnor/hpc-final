cf056ea23ada29bfebf9702881384cda00ff7a10
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-24 18:16:36 +0100

    trying to find work = 1

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -538,7 +538,7 @@ for(j = 0;j<nprocs;j++){
 
 
   //print_halo_fushion(params,local_cells,work);
-  MPI_Gatherv(&local_cells[1*params.nx],params.nx*NSPEEDS*work,MPI_FLOAT,output,rcounts,displs,MPI_FLOAT,0,MPI_COMM_WORLD;
+  MPI_Gatherv(&local_cells[1*params.nx],params.nx*NSPEEDS*work,MPI_FLOAT,output,rcounts,displs,MPI_FLOAT,0,MPI_COMM_WORLD);
   if(rank==0){
     // int t;
     // for(t =0;t<nprocs;t++){
