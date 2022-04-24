5c79a84a04aebde23c8500070628fd2742c7c769
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-24 17:42:33 +0100

    trying to find work = 1

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -202,7 +202,7 @@ int main(int argc, char* argv[])
   init_tic=tot_tic;
   initialise(paramfile, obstaclefile, &params, &cells, &tmp_cells, &obstacles, &av_vels);
 
-  i//f(params.ny/2<nprocs)nprocs = params.ny/2;
+  //f(params.ny/2<nprocs)nprocs = params.ny/2;
 
   // Obtain the group of processes in the world communicator
 MPI_Group world_group;
