029eb0e1600d4f405b7b63875c3dfafbfa83dcd3
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-28 13:15:00 +0100

    change timer code and switch Isend

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -625,7 +625,6 @@ for(j = 0;j<nprocs;j++){
   tot_toc = col_toc;
   //MPI_Barrier(MPI_COMM_WORLD);
   /* write final values and free memory */
-  if(rank==0)printf("Timer:  %f",timer);
   if(rank==0)printf("==done==    %d\n",nprocs);
   if(rank==0)printf("Reynolds number:\t\t%.12E\n", calc_reynolds(params, cells, obstacles));
   if(rank==0)printf("Elapsed Init time:\t\t\t%.6lf (s)\n",    init_toc - init_tic);
